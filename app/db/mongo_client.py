ebca8ba1e1d4a375cad7ed17010916722c3f3ece
Suduc Ionut <johnnyhd410@gmail.com>
2025-06-22 05:39:46 +0300

    Change back

--- app/db/mongo_client.py
+++
@@ -1,8 +0,0 @@
-import os
-from pymongo import MongoClient
-
-MONGO_URI = os.getenv("MONGO_KEY")
-client = MongoClient(MONGO_URI)
-
-db = client["ProductDb"]  # Numele bazei de date
-invoices_collection = db["product"]  # Numele colecției
